91a01a08cf85bd2ec8e75b5a51f66ff4027832ae
Qian <1441158517@qq.com>
2020-07-08 09:03:31 +0800

    第一次提交

---
+++ HW2_EX3/app/src/main/java/com/example/chapter2/recycler/TestData.java
@@ -0,0 +1,14 @@
+package com.example.chapter2.recycler;
+public class TestData {
+    int img;
+    String title;
+    String time;
+    String info;
+
+    public TestData(int img, String title, String info, String time) {
+        this.img = img;
+        this.title = title;
+        this.info = info;
+        this.time = time;
+    }
+}
